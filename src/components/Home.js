f8780810633a3ed8b13ddf7942557b17b5d1cba8
BeatrizSuyo <103866388+BeatrizSuyo@users.noreply.github.com>
2022-08-10 11:23:04 -0500

    creando elementos del Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,16 +4,16 @@ import { onNavigate } from '../main.js';
 
 export const Home = () => {
   const HomeDiv = document.createElement('div');
-  const buttonRegister = document.createElement('button');
-  const buttonLogin = document.createElement('button');
+  // const buttonRegister = document.createElement('button');
+  // const buttonLogin = document.createElement('button');
 
-  buttonRegister.textContent = 'Registrate';
-  buttonLogin.textContent = 'Inicia sesion';
+  // buttonRegister.textContent = 'Registrate';
+  // buttonLogin.textContent = 'Inicia sesion';
 
-  buttonRegister.addEventListener('click', () => onNavigate('/register'));
-  buttonLogin.addEventListener('click', () => onNavigate('/login'));
+  // buttonRegister.addEventListener('click', () => onNavigate('/register'));
+  // buttonLogin.addEventListener('click', () => onNavigate('/login'));
 
-  HomeDiv.appendChild(buttonRegister);
-  HomeDiv.appendChild(buttonLogin);
+  // HomeDiv.appendChild(buttonRegister);
+  // HomeDiv.appendChild(buttonLogin);
   return HomeDiv;
 };
