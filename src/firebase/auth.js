18af15540d32e94eb48327d9ef47d296f449004a
RocioTellezLopez <rociotellezlopez@gmail.com>
2022-08-10 17:04:55 -0500

    Agrega firebase auth y cambia la configuracion de firebase de html

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,1 +1,44 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.9.2/firebase-app.js';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'https://www.gstatic.com/firebasejs/9.9.2/firebase-auth.js';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyCVjOmTbyHy0FjnMVVfcrk4oU6OEs9OKh8',
+  authDomain: 'red-social-6bee5.firebaseapp.com',
+  projectId: 'red-social-6bee5',
+  storageBucket: 'red-social-6bee5.appspot.com',
+  messagingSenderId: '385355317169',
+  appId: '1:385355317169:web:79765dc3d541db35264790',
+  measurementId: 'G-T3NSZCCGQG',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+/* ---------- Firebase Auth ---------- */
+
+const provider = new GoogleAuthProvider();
+const auth = getAuth();
+signInWithPopup(auth, provider)
+  .then((result) => {
+/*     // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info. */
+    const user = result.user;
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+/*     // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ... */
+  });
+
+export const signInWithGoogle = signInWithPopup(auth, provider);
