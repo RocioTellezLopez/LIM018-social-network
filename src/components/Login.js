e27e2d98b3660a880ee0a2210050f447e1aab5ce
RocioTellezLopez <rociotellezlopez@gmail.com>
2022-08-10 20:41:53 -0500

    Agrega boton de iniciar sesion con google y evento al boton login con firebase

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,13 @@
 /* eslint-disable import/no-cycle */
+/* import { GoogleAuthProvider, signInWithPopup } from 'https://www.gstatic.com/firebasejs/9.9.2/firebase-auth.js';
+import { auth, provider } from '../firebase/auth.js'; */
+import { signInWithGoogle, signInWithEmail } from '../firebase/auth.js';
 import { onNavigate } from '../main.js';
+// import { signInWithGoogle } from '../firebase/auth.js';
 
 export const Login = () => {
   const HomeDiv = document.createElement('div');
-  HomeDiv.textContent = 'Bienvenida al login';
+  HomeDiv.className = 'homeDiv';
 
   const titleLogo = document.createElement('h1');
   titleLogo.textContent = 'HEALTHY FOOD LOVERS';
@@ -11,6 +15,12 @@ export const Login = () => {
 
   const formLogin = document.createElement('form');
   formLogin.className = 'formLogin';
+
+  /* ----- Boton para iniciar sesion con Google ----- */
+  const buttonLoginGoogle = document.createElement('input');
+  buttonLoginGoogle.type = 'button';
+  buttonLoginGoogle.value = 'Inicia sesion Google';
+  buttonLoginGoogle.id = 'buttonLoginGoogle';
 
   const inputMail = document.createElement('input');
   inputMail.type = 'email';
@@ -22,21 +32,41 @@ export const Login = () => {
 
   const buttonLogin = document.createElement('input');
   buttonLogin.type = 'button';
-  buttonLogin.value = 'Inicia sesion';//publi-home
+  buttonLogin.value = 'Inicia sesion';// publi-home
   buttonLogin.id = 'buttonLogin';
-  const buttonHome = document.createElement('button');
-  buttonHome.textContent = 'Regresar al home';
 
+  // const buttonHome = document.createElement('button');
+  // buttonHome.textContent = 'Regresar al home';
 
+  buttonLoginGoogle.addEventListener('click', () => {
 
-  buttonHome.addEventListener('click', () => onNavigate('/'));
+    signInWithGoogle();
+  
+    /* signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log('sign in');
+      })
+      .catch((err) => {
+        console.log(err);
+      }); */
+  });
 
-  HomeDiv.appendChild(buttonHome);
+  buttonLogin.addEventListener('click', () => {
+    const email = inputMail.value;
+    const password = inputPassword.value;
+
+    signInWithEmail(email, password);
+  });
+
+  // buttonHome.addEventListener('click', () => onNavigate('/'));
+
+  // HomeDiv.appendChild(buttonHome);
   HomeDiv.appendChild(titleLogo);
+  formLogin.appendChild(buttonLoginGoogle);
   formLogin.appendChild(inputMail);
   formLogin.appendChild(inputPassword);
   HomeDiv.appendChild(formLogin);
   formLogin.appendChild(buttonLogin);
-  
+
   return HomeDiv;
 };
