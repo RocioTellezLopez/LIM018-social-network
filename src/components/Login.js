bb7fc204478ac10caeb75f6d86b54bb1fc98a29d
RocioTellezLopez <rociotellezlopez@gmail.com>
2022-08-14 16:51:47 -0500

    Agrega promesas de la funcion SignInWithEmail

--- src/components/Login.js
+++ src/components/Login.js
@@ -29,11 +29,8 @@ export const Login = () => {
 
   const buttonLogin = document.createElement('input');
   buttonLogin.type = 'button';
-  buttonLogin.value = 'Inicia sesion';// publi-home
+  buttonLogin.value = 'Inicia sesion';
   buttonLogin.id = 'buttonLogin';
-
-  // const buttonHome = document.createElement('button');
-  // buttonHome.textContent = 'Regresar al home';
 
   buttonLoginGoogle.addEventListener('click', () => {
     signInWithGoogle();
@@ -43,12 +40,19 @@ export const Login = () => {
     const email = inputMail.value;
     const password = inputPassword.value;
 
-    signInWithEmail(email, password);
+    signInWithEmail(email, password)
+      .then((userCredential) => {
+      // Signed in
+        const user = userCredential.user;
+        onNavigate('/home');
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(error);
+      });
   });
 
-  // buttonHome.addEventListener('click', () => onNavigate('/'));
-
-  // HomeDiv.appendChild(buttonHome);
   HomeDiv.appendChild(titleLogo);
   formLogin.appendChild(buttonLoginGoogle);
   formLogin.appendChild(inputMail);
