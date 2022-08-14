286d83bfa1a22fedb432bdc9736af3771cffca00
RocioTellezLopez <rociotellezlopez@gmail.com>
2022-08-14 18:47:25 -0500

    Agrega font-family de google

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,27 @@
 /* eslint-disable import/no-cycle */
-// pantalla de bienvenda
 import { onNavigate } from '../main.js';
 
 export const Home = () => {
   const HomeDiv = document.createElement('div');
-  // const buttonRegister = document.createElement('button');
-  // const buttonLogin = document.createElement('button');
+  HomeDiv.className = 'homeDiv';
+  HomeDiv.classList = 'homeView';
+
+  const headerDiv = document.createElement('div');
+  headerDiv.className = 'headerDiv';
+
+  const logoDiv = document.createElement('div');
+  logoDiv.className = 'logoDiv';
+  const logoIcon = document.createElement('img');
+  logoIcon.src = '../img/comida-sana-white.png';
+
+  const titleLogo = document.createElement('h1');
+  titleLogo.textContent = 'HEALTHY FOOD LOVERS';
+  titleLogo.className = 'headerTitle';
+
+  const logOut = document.createElement('div');
+  logOut.className = 'logOut';
+  const logOutIcon = document.createElement('img');
+  logOutIcon.src = '../img/exit-free-icon.png';
 
   // buttonRegister.textContent = 'Registrate';
   // buttonLogin.textContent = 'Inicia sesion';
@@ -14,6 +30,15 @@ export const Home = () => {
   // buttonLogin.addEventListener('click', () => onNavigate('/login'));
 
   // HomeDiv.appendChild(buttonRegister);
-  // HomeDiv.appendChild(buttonLogin);
+  logoDiv.appendChild(logoIcon);
+  headerDiv.appendChild(logoDiv);
+
+  headerDiv.appendChild(titleLogo);
+
+  logOut.appendChild(logOutIcon);
+  headerDiv.appendChild(logOut);
+
+  HomeDiv.appendChild(headerDiv);
+  
   return HomeDiv;
 };
