f5da58b91b2774b10dfbfd3e7c74bf24ec645668
RocioTellezLopez <rociotellezlopez@gmail.com>
2022-08-14 16:50:45 -0500

    Elimina comentarios de los metodos de firebase

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -34,59 +34,18 @@ const auth = getAuth();
 
 signInWithPopup(auth, provider)
   .then((result) => {
-    // // This gives you a Google Access Token. You can use it to access the Google API.
-    // const credential = GoogleAuthProvider.credentialFromResult(result);
-    // const token = credential.accessToken;
-    // // The signed-in user info.
     const user = result.user;
-    // ...
   }).catch((error) => {
-    // Handle Errors here.
     const errorCode = error.code;
     const errorMessage = error.message;
-    // // The email of the user's account used.
-    // const email = error.customData.email;
-    // // The AuthCredential type that was used.
-    // const credential = GoogleAuthProvider.credentialFromError(error);
-    // // ...
   });
 
 export const signInWithGoogle = () => signInWithPopup(auth, provider);
 
 /* ---------- Firebase Auth - createUserWithEmailAndPassword ---------- */
 
-// const email = 'beatrizlaborato@gmail';
-// const password = '23456789';
-
-// console.log(auth);
-
 export const createUserWithEmail = (email, password) => createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in
-    const user = userCredential.user;
-
-    console.log('registrado con exito');
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-  });
 
 /* ---------- Firebase Auth - signInWithEmailAndPassword ---------- */
 
 export const signInWithEmail = (email, password) =>  signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in
-    const user = userCredential.user;
-    // ...
-    console.log('Iniciaste sesión');
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    alert('error de correo');
-  });
-
-// signInWithEmail('rociolaboratoria@gmai.com', '12345678');
