321e961c753df15c34f09854fbe0bfa108ae47e9
RocioTellezLopez <rociotellezlopez@gmail.com>
2022-08-14 22:02:23 -0500

    Agrega contenido a home, header, barra de navegacion y espacio para publicar

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,10 @@ import { onNavigate } from '../main.js';
 
 export const Home = () => {
   const HomeDiv = document.createElement('div');
-  HomeDiv.className = 'homeDiv';
-  HomeDiv.classList = 'homeView';
+  // HomeDiv.classList = 'homeDiv';
+  HomeDiv.classList = 'homeDiv homeView';
 
+  /* ---------- */
   const headerDiv = document.createElement('div');
   headerDiv.className = 'headerDiv';
 
@@ -23,22 +24,68 @@ export const Home = () => {
   const logOutIcon = document.createElement('img');
   logOutIcon.src = '../img/exit-free-icon.png';
 
-  // buttonRegister.textContent = 'Registrate';
-  // buttonLogin.textContent = 'Inicia sesion';
+  /* ---------- */
+  const principalContent = document.createElement('div');
+  principalContent.className = 'principalContent';
 
-  // buttonRegister.addEventListener('click', () => onNavigate('/register'));
-  // buttonLogin.addEventListener('click', () => onNavigate('/login'));
+  const publicationDiv = document.createElement('div');
+  publicationDiv.className = 'publicationDiv';
+  const textPublication = document.createElement('textarea');
+  textPublication.placeholder = '¿Qué estás pensando?';
+  const buttonPublication = document.createElement('button');
+  buttonPublication.textContent = 'Publicar';
+  buttonPublication.id = 'buttonPublication';
 
-  // HomeDiv.appendChild(buttonRegister);
+  const post = document.createElement('div');
+  post.className = 'postExample';
+
+  for (let i = 0; i < 5; i++) {
+    const post2 = document.createElement('div');
+    post2.className = 'postExample';
+    post.appendChild(post2);
+  }
+
+  /* ---------- */
+  const navDiv = document.createElement('div');
+  navDiv.className = 'navDiv';
+
+  const homeIcon = document.createElement('div');
+  homeIcon.className = 'homeIcon';
+  const homeIconImg = document.createElement('img');
+  homeIconImg.src = '../img/home-free.png';
+
+  const publicationIcon = document.createElement('div');
+  publicationIcon.className = 'publicationIcon';
+  const publicationIconImg = document.createElement('img');
+  publicationIconImg.src = '../img/edit-free-icon.png';
+
+  const profileIcon = document.createElement('div');
+  profileIcon.className = 'profileIcon';
+  const profileIconImg = document.createElement('img');
+  profileIconImg.src = '../img/user-white.png';
+
+  homeIcon.appendChild(homeIconImg);
+  publicationIcon.appendChild(publicationIconImg);
+  profileIcon.appendChild(profileIconImg);
+
+  navDiv.appendChild(homeIcon);
+  navDiv.appendChild(publicationIcon);
+  navDiv.appendChild(profileIcon);
+
   logoDiv.appendChild(logoIcon);
   headerDiv.appendChild(logoDiv);
-
   headerDiv.appendChild(titleLogo);
-
   logOut.appendChild(logOutIcon);
   headerDiv.appendChild(logOut);
 
+  principalContent.appendChild(publicationDiv);
+  principalContent.appendChild(post);
+  publicationDiv.appendChild(textPublication);
+  publicationDiv.appendChild(buttonPublication);
   HomeDiv.appendChild(headerDiv);
-  
+  HomeDiv.appendChild(principalContent);
+  // HomeDiv.appendChild(post);
+  HomeDiv.appendChild(navDiv);
+
   return HomeDiv;
 };
