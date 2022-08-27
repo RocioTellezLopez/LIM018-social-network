a2a4e1dabab1da620e4862d400e2a09755046eb0
RocioTellezLopez <rociotellezlopez@gmail.com>
2022-08-27 10:09:38 -0500

    Define la funcion signOut de firebase para utilizar en los componentes

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,10 @@
 // aqui exportaras las funciones que necesites
+import { signOutLogin } from '../firebase/auth.js';
+import { onNavigate } from '../main.js';
 
-// export const myFunction = () => {
-//   // aqui tu codigo
-//   console.log('Hola mundo!');
-// };
+export function signOutLoginFirestore() {
+  signOutLogin().then((result) => {
+    console.log('cerraste sesion');
+    onNavigate('/');
+  });
+}
