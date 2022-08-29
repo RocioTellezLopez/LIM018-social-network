a75fda6109a9568976a7b7b2fa038f85df74e6d9
RocioTellezLopez <rociotellezlopez@gmail.com>
2022-08-29 15:21:26 -0500

    Cambia console.log por alert

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,10 +1,10 @@
 // aqui exportaras las funciones que necesites
-import { signOutLogin } from '../firebase/auth.js';
+import { signOutLogin, getPost, stateChangedUser } from '../firebase/auth.js';
 import { onNavigate } from '../main.js';
 
-export function signOutLoginFirestore() {
-  signOutLogin().then((result) => {
-    console.log('cerraste sesion');
+export function signOutUser() {
+  return signOutLogin().then(() => {
+    alert('Cerraste sesion');
     onNavigate('/');
-  });
-}
+  })
+};
