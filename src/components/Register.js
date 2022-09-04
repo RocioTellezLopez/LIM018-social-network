4347dfff3801453f01820bab1dcd90c4dc177aa4
RocioTellezLopez <rociotellezlopez@gmail.com>
2022-09-03 19:14:09 -0500

    Corrige ESLint, formato de documento

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,91 +1,90 @@
 // eslint-disable-next-line import/no-cycle
-import { onNavigate } from "../main.js";
-import { createUserWithEmail, updateProfileUser } from "../firebase/auth.js";
+import { onNavigate } from '../main.js';
+import { createUserWithEmail, updateProfileUser } from '../firebase/auth.js';
 
 // Función validación de email, inputMail
 const validateEmail = (inputMail, error) => {
-  //debugger;
+  // debugger;
   const messageRegister = error;
   const validateEmailReg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g;
-  if(!validateEmailReg.test(inputMail)){
-    messageRegister.innerHTML = "El email debería tener las siguientes características: email@ejemplo.algo"
-    messageRegister.classList.add("show-messageError")
-  }else{
-    messageRegister.innerHTML = ""
-    messageRegister.classList.remove("show-messageError")
+  if (!validateEmailReg.test(inputMail)) {
+    messageRegister.innerHTML = 'El email debería tener las siguientes características: email@ejemplo.algo';
+    messageRegister.classList.add('show-messageError');
+  } else {
+    messageRegister.innerHTML = '';
+    messageRegister.classList.remove('show-messageError');
   }
-}
+};
 
 export const Register = () => {
-  const HomeDiv = document.createElement("div");
-  HomeDiv.className = "homeDiv";
+  const HomeDiv = document.createElement('div');
+  HomeDiv.className = 'homeDiv';
 
   /* ---------- */
-  const logoTitleDiv = document.createElement("div");
-  logoTitleDiv.className = "logoTitleDiv";
-  const imgLogoDiv = document.createElement("div");
-  imgLogoDiv.className = "imgLogoDiv";
-  const imgLogo = document.createElement("img");
-  imgLogo.src = "../img/comida-sana-green.png";
-  imgLogo.id = "imgLogo";
+  const logoTitleDiv = document.createElement('div');
+  logoTitleDiv.className = 'logoTitleDiv';
+  const imgLogoDiv = document.createElement('div');
+  imgLogoDiv.className = 'imgLogoDiv';
+  const imgLogo = document.createElement('img');
+  imgLogo.src = '../img/comida-sana-green.png';
+  imgLogo.id = 'imgLogo';
 
-  const titleLogo = document.createElement("h1");
-  titleLogo.textContent = "HEALTHY FOOD LOVERS";
-  titleLogo.className = "titleLogo";
+  const titleLogo = document.createElement('h1');
+  titleLogo.textContent = 'HEALTHY FOOD LOVERS';
+  titleLogo.className = 'titleLogo';
 
   /* ---------- */
 
-  const formLogin = document.createElement("form");
-  formLogin.className = "formLogin";
+  const formLogin = document.createElement('form');
+  formLogin.className = 'formLogin';
 
-  const inputName = document.createElement("input");
-  inputName.type = "text";
-  inputName.placeholder = "Ingresa tu nombre";
+  const inputName = document.createElement('input');
+  inputName.type = 'text';
+  inputName.placeholder = 'Ingresa tu nombre';
 
-  const inputMail = document.createElement("input");
-  inputMail.type = "email";
-  inputMail.placeholder = "Ingresa tu correo";
+  const inputMail = document.createElement('input');
+  inputMail.type = 'email';
+  inputMail.placeholder = 'Ingresa tu correo';
 
-  const inputPassword = document.createElement("input");
-  inputPassword.type = "password";
-  inputPassword.placeholder = "Ingresa tu contraseña";
+  const inputPassword = document.createElement('input');
+  inputPassword.type = 'password';
+  inputPassword.placeholder = 'Ingresa tu contraseña';
 
-  const buttonRegister = document.createElement("input");
-  buttonRegister.type = "button";
-  buttonRegister.value = "Registrarse";
-  buttonRegister.id = "buttonRegister";
+  const buttonRegister = document.createElement('input');
+  buttonRegister.type = 'button';
+  buttonRegister.value = 'Registrarse';
+  buttonRegister.id = 'buttonRegister';
 
-  const textLogin = document.createElement("p");
-  textLogin.textContent = "Ya tengo una cuenta";
-  textLogin.className = "textLogin";
-  const backLogin = document.createElement("a");
-  backLogin.textContent = "Inicia Sesión";
+  const textLogin = document.createElement('p');
+  textLogin.textContent = 'Ya tengo una cuenta';
+  textLogin.className = 'textLogin';
+  const backLogin = document.createElement('a');
+  backLogin.textContent = 'Inicia Sesión';
 
-  const messageRegister = document.createElement("p");
-  messageRegister.className = "mensajeRegistro";
+  const messageRegister = document.createElement('p');
+  messageRegister.className = 'mensajeRegistro';
 
-  backLogin.addEventListener("click", () => onNavigate("/login"));
+  backLogin.addEventListener('click', () => onNavigate('/login'));
 
-  //const email = inputMail.value;
-  //const password = inputPassword.value;
-  //const nameUser = inputName.value;
+  // const email = inputMail.value;
+  // const password = inputPassword.value;
+  // const nameUser = inputName.value;
 
-  //Llamado de la función validatorEmail
-  inputMail.addEventListener("change", () => {
-    validateEmail(inputMail.value, messageRegister)
+  // Llamado de la función validatorEmail
+  inputMail.addEventListener('change', () => {
+    validateEmail(inputMail.value, messageRegister);
   });
 
-  buttonRegister.addEventListener("click", () => {
-   
-    if (inputName.value === "" || inputMail.value === "" || inputPassword.value === "") {
-      return (messageRegister.innerHTML = "Ingresa los datos solicitados");
+  buttonRegister.addEventListener('click', () => {
+    if (inputName.value === '' || inputMail.value === '' || inputPassword.value === '') {
+      return (messageRegister.innerHTML = 'Ingresa los datos solicitados');
     }
     if (inputName.value.length < 3) {
-      return (messageRegister.innerHTML = "Tu nombre es muy corto: al menos 3 caracteres");
+      return (messageRegister.innerHTML = 'Tu nombre es muy corto: al menos 3 caracteres');
     }
-     
+
     if (inputPassword.value.length < 6) {
-      return (messageRegister.innerHTML = "Password como mínimo con 6 caracteres");
+      return (messageRegister.innerHTML = 'Password como mínimo con 6 caracteres');
     }
 
     createUserWithEmail(inputMail.value, inputPassword.value)
@@ -95,7 +94,7 @@ export const Register = () => {
         const uid = user.uid;
         // debugger;
         updateProfileUser(inputName.value, uid)
-        .then(() => console.log('Nombre actualizado'));
+          .then(() => console.log('Nombre actualizado'));
         console.log(user);
         console.log('Registro exitoso');
       });
